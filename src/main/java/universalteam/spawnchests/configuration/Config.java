b53a3a6e8e90768645f0b96d66ecf8a9e4572d85
big_Xplosion <bigxplosion44@gmail.com>
2014-09-25 19:37:42 +0200

    Mfixed an error because of copy paste

--- src/main/java/universalteam/spawnchests/configuration/Config.java
+++ src/main/java/universalteam/spawnchests/configuration/Config.java
@@ -1,10 +1,11 @@
 package universalteam.spawnchests.configuration;
 
 import net.minecraft.client.Minecraft;
+import universalteam.spawnchests.lib.Reference;
 
 import java.io.File;
 
 public class Config
 {
-	public static final File configLocation = new File(Minecraft.getMinecraft().mcDataDir, "config" + File.separator + "FlatBedrock");
+	public static final File configLocation = new File(Minecraft.getMinecraft().mcDataDir, "config" + File.separator + Reference.MOD_ID);
 }
